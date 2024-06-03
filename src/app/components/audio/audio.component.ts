58ba4ad790f196101c87ce399968787fc92b7c20
sp1lu <davide.rivolta@yahoo.it>
2024-06-03 22:03:10 +0200

    Audio starting on volume up interaction

--- src/app/components/audio/audio.component.ts
+++ src/app/components/audio/audio.component.ts
@@ -10,26 +10,17 @@ import { Component, Input } from '@angular/core';
 export class AudioComponent {
   public audio: HTMLAudioElement = new Audio('../../assets/audio/gloomy_sad_background_music.mp3');
 
-  private _isMuted: boolean = false;
+  private _isMuted: boolean = true;
   public get isMuted(): boolean {
     return this._isMuted;
   }
   public set isMuted(value: boolean) {
     this._isMuted = value;
-    this.audio.muted = this.isMuted;   
-  }
+    this.audio.muted = this.isMuted;
 
-  private _volume: number = 1;
-  public get volume(): number {
-    return this._volume;
-  }
-  @Input() set volume(value: number) {
-    if (!value) return;
-    this._volume = value;
-  }
-
-  public ngOnInit(): void {
-    this.audio.play();
+    if (this.audio.currentTime === 0) {            
+      this.audio.play();
+    }
   }
 
   public toggleVolume(): void {    
